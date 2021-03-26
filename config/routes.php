f8e17eba3f963a46446fbb305eeac87a49a5e30d
Piotr Kania <piotr@przyklad.pl>
2021-03-26 17:41:39 +0100

    initial commit

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/', function(Request $request, Response $response){
+        
+        $response->getBody()->write('Hello world!');
+        
+        return $response;
+
+    });
+};
